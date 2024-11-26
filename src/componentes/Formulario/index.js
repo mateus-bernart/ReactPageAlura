b1f75e21cb1fb94c2dbec4a66722fbe5c93c8cff
Mateus Bernart <mateusbernart14@gmail.com>
2024-11-26 17:29:41 -0300

    required

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -23,9 +23,13 @@ const Formulario = () => {
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+        />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereço da imagem" />
         <ListaSuspensa itens={times} label="Times" />
         <Botao>Criar Card</Botao>
       </form>
